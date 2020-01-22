6e8a86712abee68c7c0dd3831c7847e2dda30919
juniormargalho <juniormargalho@gmail.com>
2020-01-22 01:48:49 -0300

    toolbar

--- app/src/main/java/com/juniormargalho/instagram/activity/MainActivity.java
+++ app/src/main/java/com/juniormargalho/instagram/activity/MainActivity.java
@@ -1,16 +1,60 @@
 package com.juniormargalho.instagram.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.juniormargalho.instagram.R;
+import com.juniormargalho.instagram.helper.ConfiguracaoFirebase;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //configura toolbar
+        Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
+        toolbar.setTitle("Instagram");
+        setSupportActionBar(toolbar);
+
+        //configura objetos
+        autenticacao = ConfiguracaoFirebase.getInstanciaAutenticacao();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_sair :
+                deslogarUsuario();
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void deslogarUsuario(){
+        try{
+            autenticacao.signOut();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
