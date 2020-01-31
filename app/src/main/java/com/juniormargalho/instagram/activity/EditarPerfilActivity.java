87f213c4eefdca6f7a3c642c07387917ce2c7ee5
juniormargalho <juniormargalho@gmail.com>
2020-01-30 22:53:04 -0300

    editarPerfilActivity

---
+++ app/src/main/java/com/juniormargalho/instagram/activity/EditarPerfilActivity.java
@@ -0,0 +1,26 @@
+package com.juniormargalho.instagram.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+import com.juniormargalho.instagram.R;
+
+public class EditarPerfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_perfil);
+
+        //configuracoes toolbar
+        Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
+        toolbar.setTitle("Editar Perfil");
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close_black_24dp);
+
+    }
+}
--- app/src/main/java/com/juniormargalho/instagram/fragment/PerfilFragment.java
+++ app/src/main/java/com/juniormargalho/instagram/fragment/PerfilFragment.java
@@ -1,5 +1,6 @@
 package com.juniormargalho.instagram.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,17 +8,49 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.juniormargalho.instagram.R;
+import com.juniormargalho.instagram.activity.EditarPerfilActivity;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class PerfilFragment extends Fragment {
+    private ProgressBar progressBarPerfil;
+    private CircleImageView imagePerfil;
+    public GridView gridViewPerfil;
+    private TextView textPublicacoes, textSeguidores, textSeguindo;
+    private Button buttonEditarPerfil;
 
     public PerfilFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_perfil, container, false);
+        View view = inflater.inflate(R.layout.fragment_perfil, container, false);
+
+        //configuracoes componentes
+        gridViewPerfil = view.findViewById(R.id.gridViewPerfil);
+        progressBarPerfil = view.findViewById(R.id.progressBarPerfil);
+        imagePerfil = view.findViewById(R.id.imagePerfil);
+        textPublicacoes = view.findViewById(R.id.textPublicacoes);
+        textSeguidores = view.findViewById(R.id.textSeguidores);
+        textSeguindo = view.findViewById(R.id.textSeguindo);
+        buttonEditarPerfil = view.findViewById(R.id.buttonEditarPerfil);
+
+        //abrir edicao perfil
+        buttonEditarPerfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(), EditarPerfilActivity.class);
+                startActivity(i);
+            }
+        });
+
+        return view;
     }
 
 }
