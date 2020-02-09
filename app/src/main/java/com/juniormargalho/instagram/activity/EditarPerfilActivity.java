1a95f19c36080a0fd44b5c0a3e4f188866292f07
juniormargalho <juniormargalho@gmail.com>
2020-02-08 21:40:36 -0300

    imagemPerfilUploadStorage

--- app/src/main/java/com/juniormargalho/instagram/activity/EditarPerfilActivity.java
+++ app/src/main/java/com/juniormargalho/instagram/activity/EditarPerfilActivity.java
@@ -1,19 +1,32 @@
 package com.juniormargalho.instagram.activity;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.juniormargalho.instagram.R;
+import com.juniormargalho.instagram.helper.ConfiguracaoFirebase;
 import com.juniormargalho.instagram.helper.UsuarioFirebase;
 import com.juniormargalho.instagram.model.Usuario;
+
+import java.io.ByteArrayOutputStream;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -23,6 +36,8 @@ public class EditarPerfilActivity extends AppCompatActivity {
     private TextInputEditText editNomePerfil, editEmailPerfil;
     private Button buttonSalvarAlteracoes;
     private Usuario usuarioLogado;
+    private static final int SELECAO_GALERIA = 200;
+    private StorageReference storageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +46,7 @@ public class EditarPerfilActivity extends AppCompatActivity {
 
         //configuracoes iniciais
         usuarioLogado = UsuarioFirebase.getDadosUsuarioLogado();
+        storageRef = ConfiguracaoFirebase.getReferenciaStorage();
 
         //configuracoes toolbar
         Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
@@ -63,6 +79,61 @@ public class EditarPerfilActivity extends AppCompatActivity {
             }
         });
 
+        //alterar foto do usuario
+        textAlterarFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                if( i.resolveActivity(getPackageManager()) != null ){
+                    startActivityForResult(i, SELECAO_GALERIA);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if( resultCode == RESULT_OK ){
+            Bitmap imagem = null;
+
+            try{
+                switch (requestCode){
+                    case SELECAO_GALERIA :
+                        Uri localImagemSelecionada = data.getData();
+                        imagem = MediaStore.Images.Media.getBitmap( getContentResolver(), localImagemSelecionada );
+                        break;
+                }
+
+                if( imagem != null ){
+                    imageEditarPerfil.setImageBitmap( imagem );
+
+                    //salvando no firebase storage
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    imagem.compress(Bitmap.CompressFormat.JPEG, 70, baos);
+                    byte[] dadosImagem = baos.toByteArray();
+                    StorageReference imagemRef = storageRef.child("imagens").child("perfil").child("<id-usuario>.jpeg");
+                    UploadTask uploadTask = imagemRef.putBytes( dadosImagem );
+                    uploadTask.addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(EditarPerfilActivity.this, "Erro ao fazer upload da imagem!", Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Toast.makeText(EditarPerfilActivity.this, "Sucesso ao fazer upload da imagem!", Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
+
+                }
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 
     public void inicializarComponentes(){
--- app/src/main/java/com/juniormargalho/instagram/helper/ConfiguracaoFirebase.java
+++ app/src/main/java/com/juniormargalho/instagram/helper/ConfiguracaoFirebase.java
@@ -3,10 +3,13 @@ package com.juniormargalho.instagram.helper;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class ConfiguracaoFirebase {
     private static DatabaseReference referenciaDatabase;
     private static FirebaseAuth instanciaAutenticacao;
+    private static StorageReference referenciaStorage;
 
     public static DatabaseReference getReferenciaDatabase(){
         if( referenciaDatabase == null ){
@@ -20,6 +23,13 @@ public class ConfiguracaoFirebase {
             instanciaAutenticacao = FirebaseAuth.getInstance();
         }
         return instanciaAutenticacao;
+    }
+
+    public static StorageReference getReferenciaStorage(){
+        if(referenciaStorage == null){
+            referenciaStorage = FirebaseStorage.getInstance().getReference();
+        }
+        return referenciaStorage;
     }
 
 }
