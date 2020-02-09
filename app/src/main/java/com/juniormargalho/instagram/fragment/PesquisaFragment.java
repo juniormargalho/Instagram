79b8013bd8c3820ec788c14c50bd19691bb14381
juniormargalho <juniormargalho@gmail.com>
2020-02-09 20:05:11 -0300

    layoutFragment_pesquisa

--- app/src/main/java/com/juniormargalho/instagram/fragment/PesquisaFragment.java
+++ app/src/main/java/com/juniormargalho/instagram/fragment/PesquisaFragment.java
@@ -2,7 +2,9 @@ package com.juniormargalho.instagram.fragment;
 
 import android.os.Bundle;
 
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,13 +13,34 @@ import android.view.ViewGroup;
 import com.juniormargalho.instagram.R;
 
 public class PesquisaFragment extends Fragment {
+    private SearchView searchViewPesquisa;
+    private RecyclerView recyclerPesquisa;
 
     public PesquisaFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_pesquisa, container, false);
+        View view = inflater.inflate(R.layout.fragment_pesquisa, container, false);
+
+        searchViewPesquisa = view.findViewById(R.id.searchViewPesquisa);
+        recyclerPesquisa = view.findViewById(R.id.recyclerPesquisa);
+
+        //configuracao searchView
+        searchViewPesquisa.setQueryHint("Buscar usuários");
+        searchViewPesquisa.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        return view;
     }
 
 }
