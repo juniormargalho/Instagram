cc9a2ad0efb2b1aafdeec1ba0a05ec6102be9cd9
juniormargalho <juniormargalho@gmail.com>
2020-02-27 20:51:24 -0300

    interfaceFiltro

--- app/src/main/java/com/juniormargalho/instagram/activity/FiltroActivity.java
+++ app/src/main/java/com/juniormargalho/instagram/activity/FiltroActivity.java
@@ -1,10 +1,14 @@
 package com.juniormargalho.instagram.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
 
 import com.juniormargalho.instagram.R;
@@ -21,6 +25,13 @@ public class FiltroActivity extends AppCompatActivity {
         //Inicializar componentes
         imageFotoEscolhida = findViewById(R.id.imageFotoEscolhida);
 
+        //configuracoes toolbar
+        Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
+        toolbar.setTitle("Filtros");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close_black_24dp);
+
         //recupera a imagem escolhida pelo usuario
         Bundle bundle = getIntent().getExtras();
         if(bundle != null){
@@ -28,6 +39,30 @@ public class FiltroActivity extends AppCompatActivity {
             imagem = BitmapFactory.decodeByteArray(dadosImagem, 0, dadosImagem.length);
             imageFotoEscolhida.setImageBitmap(imagem);
         }
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.ic_salvar_postagem :
+
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_filtro, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return false;
     }
 }
